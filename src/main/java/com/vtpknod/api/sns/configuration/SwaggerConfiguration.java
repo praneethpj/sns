c07ad544478afebe5f061c4be19cf929d796854e
praneethpj <ifeathern@gmail.com>
2020-10-07 12:57:11 +0530

    add swager mofifications

--- src/main/java/com/vtpknod/api/sns/configuration/SwaggerConfiguration.java
+++ src/main/java/com/vtpknod/api/sns/configuration/SwaggerConfiguration.java
@@ -1,4 +1,21 @@
 package com.vtpknod.api.sns.configuration;
 
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+
+@Configuration
+
 public class SwaggerConfiguration {
+
+
+
+    private void apiInfo() {
+        return new ApiInfoBuilder().title("KNOD Web Application REST API")
+                .description("KNOD User REST  API reference for developers")
+                .termsOfServiceUrl("http://oensys.com")
+                .license("KNOD License")
+                .licenseUrl("dev@oensys.com").version("1.0").build();
+    }
 }
