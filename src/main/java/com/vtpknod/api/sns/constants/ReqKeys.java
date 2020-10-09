c2196c12ce6963b19d23789bca227a44367f23c1
Pubudu Praneeth Jayarathna <ifeathern@gmail.com>
2020-10-09 17:00:40 +0530

    Update ReqKeys.java

--- src/main/java/com/vtpknod/api/sns/constants/ReqKeys.java
+++ src/main/java/com/vtpknod/api/sns/constants/ReqKeys.java
@@ -2,10 +2,10 @@ package com.vtpknod.api.sns.constants;
 
 public class ReqKeys {
 
-    public  static final String SECRETE_KEY="P9MPSv80MniCnfSxGScN5rgpWA7W82+/A29lRbnH";
-    public static final String ACCESS_KEY="AKIAWKXR6H5SANBGYD4U";
+    public  static final String SECRETE_KEY="xxxx";
+    public static final String ACCESS_KEY="xxxxx";
 
-    public static final String Topic_ARN="arn:aws:sns:us-east-2:435372834660:forsqs";
+    public static final String Topic_ARN="xxxx";
 
 
 
